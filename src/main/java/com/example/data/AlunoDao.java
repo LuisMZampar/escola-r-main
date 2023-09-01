7862c4a703c7e413a5ec6d19e6289359b2ce8049
LuisMZampar <rm550531@fiap.com.br>
2023-08-31 21:00:56 -0300

    Realizando projeto com Dao

---
+++ src/main/java/com/example/data/AlunoDao.java
@@ -0,0 +1,53 @@
+package com.example.data;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.model.Aluno;
+
+public class AlunoDao {
+    final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+    final String USER = "XX"; //TROCAR
+    final String PASS = "XX"; //TROCAR
+
+    public void inserir(Aluno aluno) throws SQLException {
+
+        var conexao = DriverManager.getConnection(URL, USER, PASS);
+        var sql = "INSERT INTO alunos (nome, turma, rm) VALUES (?, ?, ?) ";
+        var comando = conexao.prepareStatement(sql);
+        comando.setString(1, aluno.nome());
+        comando.setString(2, aluno.turma());
+        comando.setInt(3, aluno.rm());
+        comando.executeUpdate();
+
+        conexao.close();
+
+    }
+
+    public List<Aluno> buscarTodos() throws SQLException {
+
+        var alunos = new ArrayList<Aluno>();
+        var conexao = DriverManager.getConnection(URL, USER, PASS);
+        var rs = conexao.createStatement().executeQuery("SELECT * FROM alunos ORDER BY nome");
+
+        while (rs.next()) {
+            alunos.add(new Aluno(
+                    rs.getString("nome"),
+                    rs.getString("turma"),
+                    rs.getInt("rm")));
+        }
+
+        conexao.close();
+        return alunos;
+    }
+
+    public void apagar(Aluno aluno) throws SQLException {
+        var con = DriverManager.getConnection(USER, URL, PASS);
+        var stm = con.prepareStatement("DELETE FROM alunos WHERE rm=?");
+        stm.setInt(1, aluno.rm());
+        stm.executeUpdate();
+        con.close();
+    }
+}
